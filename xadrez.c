f6f2b56cd3deb183dbe9306619f6d3c1a558dc49
Jefferson Portela Pereira <jeffersonportela@live.com>
2025-08-28 03:42:29 -0400

    correção desafio novato.

--- xadrez.c
+++ xadrez.c
@@ -7,40 +7,37 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     
-    //Declaração do numero inicial de casas que cada peça irá se movimentar.
-    int movimentosTorre = 5;
-    int movimentosBispo = 1;
-    int movimentosRainha = 1;
+     //Declaração do numero de casas que cada peça irá se movimentar.
+    int totalDeMovimentosTorre = 5;
+    int totalDeMovimentosBispo = 5;
+    int totalDeMovimentosRainha = 8;
     
     //repetições para movimentos da Torre.
     printf("-- Movimentos da Torre -- \n");
-    for(int i = 0; i < movimentosTorre; i++){
+    for(int i = 0; i < totalDeMovimentosTorre; i++){
         printf("Torre se movimenta uma casa para a direita. \n");
     }
-    //descrição final de quantas casas a peça de movimentou.
-    printf("-- numero de casas que a Torre se movimentou(%d) -- \n", movimentosTorre);
+    printf("-- numero de casas que a Torre se movimentou(%d) -- \n", totalDeMovimentosTorre);
     
-    //repetições para movimentos do Bispo.
+    //repetições para movimentos do bispo.
     printf("-- Movimentos do Bispo -- \n");
-    while(movimentosBispo < 5 ){
-        movimentosBispo++;
+    int movimentoBispo = 0;
+    while(movimentoBispo < totalDeMovimentosBispo ){
+        movimentoBispo++;
         printf("Bispo se movimenta uma casa para a cima e uma casa para a direita. \n");
     }
-    //descrição final de quantas casas a peça de movimentou.
-    printf("-- numero de casas que o Bispo se movimentou(%d) -- \n", movimentosBispo);
+    printf("-- numero de casas que o Bispo se movimentou(%d) -- \n", totalDeMovimentosBispo);
     
-    //repetições para movimentos da Rainha.
+    //repetições para movimentos da rinha.
     printf("-- Movimentos da Rainha -- \n");
-
+    int movimentoRainha = 0;
     do{
-        movimentosRainha++;
+        movimentoRainha++;
         printf("Rainha se movimenta uma casa para a esquerda. \n");
-    }while(movimentosRainha < 8);
-
-    //descrição final de quantas casas a peça de movimentou.
-    printf("-- numero de casas que a Rainha se movimentou(%d) -- \n", movimentosRainha);
+    }while(movimentoRainha< totalDeMovimentosRainha);
+    printf("-- numero de casas que a Rainha se movimentou(%d) -- \n", totalDeMovimentosRainha);
     
-
+ 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
